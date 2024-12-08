5214e13f429691caaff87c09b2b3fbb348665f3f
Immu <shaikimam37302@gmail.com>
2024-12-08 23:32:06 +0530

    Basic operations of Circular LinkedList

---
+++ LinkedList/Circular_LinkedList/Basic_Operations_of_Circular_LinkedList.C
@@ -0,0 +1,120 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Node structure
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+// Function to create a new node
+struct Node* createNode(int data) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->next = NULL;
+    return newNode;
+}
+
+// Function to append a node to the circular linked list
+void append(struct Node** head, int data) {
+    struct Node* newNode = createNode(data);
+
+    if (*head == NULL) {
+        newNode->next = newNode;
+        *head = newNode;
+    } else {
+        struct Node* temp = *head;
+        while (temp->next != *head) {
+            temp = temp->next;
+        }
+        temp->next = newNode;
+        newNode->next = *head;
+    }
+
+    printf("Appended %d to the list.\n", data);
+}
+
+// Function to delete a node at the end of the list
+void deleteAtEnd(struct Node** head) {
+    if (*head == NULL) {
+        printf("List is empty. Nothing to delete.\n");
+        return;
+    }
+
+    struct Node* temp = *head;
+    if (temp->next == *head) {
+        *head = NULL;
+        free(temp);
+        printf("Deleted the only node in the list.\n");
+        return;
+    }
+
+    struct Node* prev = NULL;
+    while (temp->next != *head) {
+        prev = temp;
+        temp = temp->next;
+    }
+    prev->next = *head;
+
+    free(temp);
+    printf("Deleted node at the end.\n");
+}
+
+// Function to display the list
+void display(struct Node* head) {
+    if (head == NULL) {
+        printf("List is empty.\n");
+        return;
+    }
+
+    struct Node* temp = head;
+    printf("List: ");
+
+    do {
+        printf("%d ", temp->data);
+        temp = temp->next;
+    } while (temp != head);
+
+    printf("\n");
+}
+
+// Main function
+int main() {
+    struct Node* head = NULL;
+    int choice, value;
+
+    while (1) {
+        // Display the menu
+        printf("\nMenu:\n");
+        printf("1. Append a node\n");
+        printf("2. Delete a node at the end\n");
+        printf("3. Display the list\n");
+        printf("4. Exit\n");
+
+        // Get user's choice
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+
+        switch (choice) {
+            case 1:
+                printf("Enter value to append: ");
+                scanf("%d", &value);
+                append(&head, value);
+                break;
+            case 2:
+                deleteAtEnd(&head);
+                break;
+            case 3:
+                display(head);
+                break;
+            case 4:
+                printf("Exiting...\n");
+                return 0;
+            default:
+                printf("Invalid choice. Try again.\n");
+        }
+    }
+
+    return 0;
+}
+
