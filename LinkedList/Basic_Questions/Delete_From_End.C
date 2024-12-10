16f73d1230d2857d39ccfb49ca45b5d400c411d6
Immu <shaikimam37302@gmail.com>
2024-12-10 22:01:51 +0530

    All basic Questions

--- LinkedList/Basic_Questions/Delete_From_End.C
+++ LinkedList/Basic_Questions/Delete_From_End.C
@@ -20,25 +20,17 @@ struct node* create_node(int data) {
 }
 
 // Function to display the linked list
-void display_list(struct node *head) {
+void display(struct node *head) {
     if (head == NULL) {
         printf("The list is empty.\n");
         return;
     }
     struct node *temp = head;
     printf("Linked List: ");
-    while (temp != NULL) {
-        printf("%d ", temp->data);
+    while (temp != NULL) {          //we have to get the data till last node
+        printf("%d ", temp->data);  //so that we are not comparing with the link
         temp = temp->link;
     }
-}
-
-// Insert at the beginning
-struct node* insert_at_beginning(struct node *head, int data) {
-    struct node *new_node = create_node(data);
-    new_node->link = head;
-    head =new_node;
-    return new_node;
 }
 
 // Insert at the end
@@ -48,32 +40,11 @@ struct node* insert_at_end(struct node *head, int data) {
         return new_node;
     }
     struct node *temp = head;
-    while (temp->link != NULL) {
-        temp = temp->link;
+    while (temp->link != NULL) {    //to find the last node
+        temp = temp->link;          //will upadte the new node address in last node link
     }
     temp->link = new_node;
-    return head;
-}
-
-// Insert at a specific position
-struct node* insert_at_position(struct node *head, int data, int position) {
-    struct node *new_node = create_node(data);
-    if (position == 1) {
-        new_node->link = head;
-        return new_node;
-    }
-    struct node *temp = head;
-    for (int i = 1; i < position - 1 && temp != NULL; i++) {
-        temp = temp->link;
-    }
-    if (temp == NULL) {
-        printf("Invalid position!\n");
-        free(new_node);
-        return head;
-    }
-    new_node->link = temp->link;
-    temp->link = new_node;
-    return head;
+    return head;            //it will return all the nodes created
 }
 
 //delete from the end
@@ -84,7 +55,7 @@ struct node* delete_from_end(struct node *head) {
         return NULL;
     }
     if (head->link == NULL) { // Only one node in the list
-        free(head);
+        free(head);     //removes the value stored in the variable
         return NULL;
     }
     struct node *temp = head;
@@ -102,48 +73,13 @@ struct node* delete_from_end(struct node *head) {
 // Main function
 int main() {
 struct node *head = NULL;
-int choice, data, position;
-while (1) {
-    printf("\nMenu:\n");
-    printf("1. Insert at the beginning\n");
-    printf("2. Insert at the end\n");
-    printf("3. Insert at a specific position\n");
-    printf("4. Delete at the End\n");
-    printf("5. Display Linked List\n");
-    printf("6. Exit\n");
-    printf("Enter your choice: ");
-    scanf("%d", &choice);
-    switch (choice) {
-        case 1:
-            printf("Enter the value to insert at the beginning: ");
-            scanf("%d", &data);
-            head = insert_at_beginning(head, data);
-            break;
-        case 2:
-            printf("Enter the value to insert at the end: ");
-            scanf("%d", &data);
-            head = insert_at_end(head, data);
-            break;
-        case 3:
-            printf("Enter the value to insert: ");
-            scanf("%d", &data);
-            printf("Enter the position: ");
-            scanf("%d", &position);
-            head = insert_at_position(head, data, position);
-            break;
-        case 4:
-            head = delete_from_end(head);
-            printf("Dleted In the End\n");
-            break;
-        case 5:
-            display_list(head);
-            break;
-        case 6:
-            printf("Exiting the program.\n");
-            exit(0);
-        default:
-            printf("Invalid choice! Please try again.\n");
-        }
-    }
+head = insert_at_end(head,10);
+head = insert_at_end(head,20);
+head = insert_at_end(head,30);  
+head = insert_at_end(head,40);
+display(head);
+head=delete_from_end(head);
+printf("\n");
+display(head);
 return 0;
 }
